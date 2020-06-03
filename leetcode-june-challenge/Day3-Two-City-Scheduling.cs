ab05cd3ba1fc11ba1ee47c4ca7b2c08d1f4e14e4
Assylzhan <strelokask@gmail.com>
2020-06-03 17:29:12 +0600

    Day 3

---
+++ leetcode-june-challenge/Day3-Two-City-Scheduling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace leetcode_june_challenge
+{
+    class Day3_Two_City_Scheduling
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            List<int> list = new List<int>();
+            foreach (var cost in costs) {
+                list.Add(cost[0] - cost[1]);
+            }
+
+            var sortedList = list
+                .Select((x, i) => new KeyValuePair<int, int>(x, i))
+                .OrderBy(x => x.Key)
+                .ToList();
+
+            int result = 0;
+            for(int i=0; i < sortedList.Count; i++)
+            {
+                if (i > sortedList.Count / 2) {
+                    result += costs[sortedList[i].Value][1];
+                }
+                else result += costs[sortedList[i].Value][0];
+            }
+           
+
+            return result;
+        }
+    }
+}
