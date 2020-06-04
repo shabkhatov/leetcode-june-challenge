d752193eef80d3a741ec80fd007c5d83f3b813aa
Assylzhan <strelokask@gmail.com>
2020-06-04 13:51:13 +0600

    Day4

---
+++ leetcode-june-challenge/Day4-Reverse-String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode_june_challenge
+{
+    class Day4_Reverse_String
+    {
+        public void ReverseString(char[] s)
+        {
+            int n = s.Length - 1;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                var temp = s[i];
+                s[i] = s[n - i];
+                s[n - i] = temp;
+            }
+        }
+    }
+}
