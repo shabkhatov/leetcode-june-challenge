e6af92f4200fe9113793ca0ddadca22bb7939d87
Assylzhan <strelokask@gmail.com>
2020-06-09 13:11:13 +0600

    Day 9

---
+++ leetcode-june-challenge/Day9-In-Subsequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode_june_challenge
+{
+    class Day9_In_Subsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int j = 0;
+            for (int i = 0; j < s.Length && i < t.Length; i++)
+            {
+                if (s[j] == t[i])
+                {
+                    j++;
+                }
+            }
+
+            return j == s.Length;
+        }
+    }
+}
